d628417d2624e23fb41fa96e48ae6974e45f06d9
Celal AKSU <celalaksu@gmail.com>
2024-11-09 23:26:35 +0300

    degişiklik

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     return ( 
         <div className="footer">
             <h1> <a href="https://www.youtube.com/@eemcs">eemcs academy</a></h1>
+            <h3><a href="https://www.linkedin.com/in/cllaksu/">Celal AKSU</a></h3>
         </div>
     );
 }
